f52f3ed0ec3b8b1e65167ac213b5c3cfba364bb4
chokii4352 <gaon4352@gmail.com>
2026-03-31 22:47:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
-    domains: ['your-project.supabase.co'],  // Supabase Storage 도메인으로 변경
+    domains: ['your-project.supabase.co'],
   },
 }
 
