1cd2a4940074c2339bc4db2096a14a9ec610cf40
chokii4352 <gaon4352@gmail.com>
2026-04-01 12:25:43 +0900

    Update page.tsx

--- app/store/notices/page.tsx
+++ app/store/notices/page.tsx
@@ -1,5 +1,4 @@
-export const dynamic = 'force-dynamic'
-  
+export const dynamic = 'force-dynamic'  
 import { getSession } from '@/lib/auth'
 import { supabaseAdmin } from '@/lib/supabase'
 import Link from 'next/link'
